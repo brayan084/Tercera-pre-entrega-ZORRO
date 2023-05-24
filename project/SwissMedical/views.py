6b858ea56e67a16428ebee1474f16924c4672f9a
brayan <zorrobrayan0@gmail.com>
2023-05-24 13:34:43 -0300

    update

--- project/SwissMedical/views.py
+++ project/SwissMedical/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from .forms import MedicosForm, PacientesForm, CitasForm, recetasForm
-from .models import Medicos, Pacientes
+from .models import Medicos, Pacientes, Citas, recetas
 
 # Create your views here.
 def index(request):
     pacientes_registrados = Pacientes.objects.all()
     medicos_registrados = Medicos.objects.all()
+    citas_registradas = Citas.objects.all()
     datos = {
         'medicos': medicos_registrados,
-        'pacientes': pacientes_registrados
+        'pacientes': pacientes_registrados,
+        'citas': citas_registradas,
     }
     return render(request, 'SwissMedical/index.html', datos)
 
@@ -38,6 +40,7 @@ def citas(request):
         form = CitasForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('index')
     else:
         form = CitasForm()
     return render(request, 'SwissMedical/crearCitas.html', {'form': form})
@@ -47,6 +50,7 @@ def recetas(request):
         form = recetasForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('index')
     else:
         form = recetasForm()
     return render(request, 'SwissMedical/crearRecetas.html', {'form': form})
